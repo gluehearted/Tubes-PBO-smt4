6a0af7eedf4cfd0039a7c68a49c5982f7e1841d6
gluehearted <139973575+gluehearted@users.noreply.github.com>
2025-05-27 22:43:10 +0700

    Add files via upload

---
+++ Tubes Order Makanan/src/exception/DuplicateUsernameException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author Gabriel Prakosa A
+ */
+public class DuplicateUsernameException extends Exception {
+    public DuplicateUsernameException(String message) {
+        super(message);
+    }
+}
---
+++ Tubes Order Makanan/src/exception/EmptyCartException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author Gabriel Prakosa A
+ */
+public class EmptyCartException extends Exception{
+    public EmptyCartException(String message) {
+        super(message);
+    }
+}
